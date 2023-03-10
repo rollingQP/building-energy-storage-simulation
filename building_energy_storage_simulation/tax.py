cf817b94541033d155f3482b52a78dbd6cadce16
rollingQP <59548978+rollingQP@users.noreply.github.com>
2023-03-10 12:20:04 +0100

    create tax.py

---
+++ building_energy_storage_simulation/tax.py
@@ -0,0 +1,23 @@
+class Tax:
+    """
+    The Tax class
+    """
+
+    def __init__(self,
+                 method = "constant",
+                 tax_table = [39.42],
+                 ):
+        self.method = method
+        self.tax_table = tax_table
+        
+    def calc_tax(self, electricity_draw_from_grid):
+        if self.method == "constant":
+            return electricity_draw_from_grid * self.tax_table[0]
+        elif self.method == "tiered":
+            pass
+
+    def reset(self):
+        """
+        Resetting the `state_of_charge` of the battery to the `initial_state_of_charge`.
+        """
+        pass
